15d8075f2e4dc2fe61f01cd4f37f96dc28582409
baydouna <ali.baydoun@etu.univ-grenoble-alpes.fr>
2024-04-03 13:32:14 +0200

    v1 of dataframe connstructors

---
+++ src/main/java/DataFrame.java
@@ -0,0 +1,156 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Hashtable;
+
+/**
+ * Represents a DataFrame, a two-dimensional labeled data structure with columns of potentially different types.
+ * Provides methods to manipulate and analyze tabular data.
+ */
+public class DataFrame {
+
+    private final Hashtable<String, Object[][]> columns;
+    private final ArrayList<Object[]> rows;
+    private final ArrayList<String> labels;
+
+    /**
+     * Constructs a DataFrame from a 2D array of Objects.
+     *
+     * @param data The 2D array representing the DataFrame. The first row should contain column labels.
+     * @throws IllegalArgumentException if the input data has fewer than one row or one column.
+     */
+    public DataFrame(Object[][] data) {
+        if (data.length < 1 || data[0].length < 1) {
+            throw new IllegalArgumentException("Incorrect input data format.");
+        }
+
+        columns = new Hashtable<>();
+        rows = new ArrayList<>();
+        labels = new ArrayList<>();
+
+        String[] columnLabels = new String[data[0].length];
+        for (int i = 0; i < data[0].length; i++) {
+            columnLabels[i] = data[0][i].toString();
+            labels.add(columnLabels[i]);
+        }
+
+        for (int i = 0; i < columnLabels.length; i++) {
+            String columnLabel = columnLabels[i];
+            Object[][] columnData = new Object[data.length - 1][1];
+            for (int j = 1; j < data.length; j++) {
+                columnData[j - 1][0] = data[j][i];
+            }
+            columns.put(columnLabel, columnData);
+        }
+
+        rows.addAll(Arrays.asList(data).subList(1, data.length));
+    }
+
+    /**
+     * Constructs a DataFrame from column labels and corresponding row values.
+     *
+     * @param columns_labels The labels of the columns.
+     * @param rows_values    The values of the rows, organized as a 2D array.
+     * @throws IllegalArgumentException if the length of column labels is not equal to the number of columns in rows_values.
+     */
+    public DataFrame(String[] columns_labels, Object[][] rows_values) {
+
+        if (rows_values.length == 0 || columns_labels.length == 0 ||
+                columns_labels.length != rows_values[0].length) {
+            throw new IllegalArgumentException("Incorrect input data format.");
+        }
+
+        columns = new Hashtable<>();
+        rows = new ArrayList<>();
+        labels = new ArrayList<>();
+
+        rows.addAll(Arrays.asList(rows_values));
+
+        for (int i = 0; i < columns_labels.length; i++) {
+            String columnLabel = columns_labels[i];
+            labels.add(columnLabel);
+            Object[][] columnData = new Object[rows_values.length][1];
+            for (int j = 0; j < rows_values.length; j++) {
+                columnData[j][0] = rows_values[j][i];
+            }
+            columns.put(columnLabel, columnData);
+        }
+    }
+
+    /**
+     * Returns a list of column labels in the DataFrame.
+     *
+     * @return An ArrayList containing the column labels.
+     */
+    public ArrayList<String> getColumnLabels() {
+        return this.labels;
+    }
+
+    /**
+     * Returns a list of values of the column with the given label.
+     *
+     * @param columnName The label of the column.
+     * @return A list containing the values of the specified column.
+     * @throws IllegalArgumentException if the column with the given label does not exist.
+     */
+    public ArrayList<Object> getColumnValues(String columnName) {
+        if (!columns.containsKey(columnName)) {
+            throw new IllegalArgumentException("Column with label " + columnName + " does not exist.");
+        }
+
+        Object[][] columnData = columns.get(columnName);
+        ArrayList<Object> columnValues = new ArrayList<>();
+
+        for (Object[] rowData : columnData) {
+            columnValues.add(rowData[0]);
+        }
+
+        return columnValues;
+    }
+
+    /**
+     * Returns an ArrayList containing the values of the row at the specified index.
+     *
+     * @param index The index of the row.
+     * @return An ArrayList containing the values of the row.
+     * @throws IndexOutOfBoundsException if the index is out of range.
+     */
+    public ArrayList<Object> getRowValues(int index) {
+        if (index < 0 || index >= rows.size()) {
+            throw new IndexOutOfBoundsException("Index is out of range.");
+        }
+
+        Object[] rowData = rows.get(index);
+        ArrayList<Object> rowValues = new ArrayList<>();
+
+        Collections.addAll(rowValues, rowData);
+
+        return rowValues;
+    }
+
+    /**
+     * Prints the DataFrame to the console.
+     * The DataFrame is printed in a tabular format with column labels and corresponding row values.
+     */
+    public void printDataFrame() {
+
+        System.out.print(" |");
+        for (String column : columns.keySet()) {
+            System.out.print(" " + column + " |");
+        }
+        System.out.println();
+
+        for (int i = 0; i <= columns.size(); i++) {
+            System.out.print("------");
+        }
+        System.out.println();
+
+        for (Object[] row : rows) {
+            System.out.print(" |");
+            for (Object value : row) {
+                System.out.print(" " + value + " |");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/test/java/DataFrameTest.java
@@ -0,0 +1,157 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class DataFrameTest {
+
+    @Test
+    public void testGetRowValues() {
+        String[] columnsLabels = {"Name", "Age", "Country"};
+        Object[][] rowsValues = {
+                {"Ali", 21, "Lebanon"},
+                {"Serge", 24, "Armenia"},
+                {"Jorane", 23, "France"},
+                {"Noemie", 23, "France"}
+        };
+        DataFrame df = new DataFrame(columnsLabels, rowsValues);
+
+        ArrayList<Object> expectedRowValues = new ArrayList<>();
+        expectedRowValues.add("Serge");
+        expectedRowValues.add(24);
+        expectedRowValues.add("Armenia");
+
+        assertEquals(expectedRowValues, df.getRowValues(1));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetRowValuesWithInvalidIndex() {
+        String[] columnsLabels = {"Name", "Age", "Country"};
+        Object[][] rowsValues = {
+                {"Ali", 21, "Lebanon"},
+                {"Serge", 24, "Armenia"},
+                {"Jorane", 23, "France"},
+                {"Noemie", 23, "France"}
+        };
+        DataFrame df = new DataFrame(columnsLabels, rowsValues);
+
+        df.getRowValues(10);
+    }
+
+    @Test
+    public void testGetColumnLabels() {
+        String[] columnsLabels = {"Name", "Age", "Country"};
+        Object[][] rowsValues = {
+                {"Ali", 21, "Lebanon"},
+                {"Serge", 24, "Armenia"},
+                {"Jorane", 23, "France"},
+                {"Noemie", 23, "France"}
+        };
+        DataFrame df = new DataFrame(columnsLabels, rowsValues);
+
+        ArrayList<String> expectedColumnLabels = new ArrayList<>();
+        expectedColumnLabels.add("Name");
+        expectedColumnLabels.add("Age");
+        expectedColumnLabels.add("Country");
+
+        assertEquals(expectedColumnLabels, df.getColumnLabels());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithEmptyData() {
+        Object[][] data = {};
+        DataFrame df = new DataFrame(data);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithEmptyRow() {
+        Object[][] data = {{}};
+        DataFrame df = new DataFrame(data);
+    }
+
+    @Test
+    public void testConstructorWithValidData() {
+        Object[][] data = {
+                {"Name", "Age", "Country"},
+                {"Ali", 21, "Lebanon"},
+                {"Serge", 24, "Armenia"},
+                {"Jorane", 23, "France"},
+                {"Noemie", 23, "France"}
+        };
+
+        DataFrame df = new DataFrame(data);
+
+        assertEquals("Name", df.getColumnLabels().get(0));
+        assertEquals("Age", df.getColumnLabels().get(1));
+        assertEquals("Country", df.getColumnLabels().get(2));
+
+        assertEquals("Ali", df.getColumnValues("Name").get(0));
+        assertEquals(21, df.getColumnValues("Age").get(0));
+        assertEquals("Lebanon", df.getColumnValues("Country").get(0));
+
+        assertEquals("Serge", df.getColumnValues("Name").get(1));
+        assertEquals(24, df.getColumnValues("Age").get(1));
+        assertEquals("Armenia", df.getColumnValues("Country").get(1));
+
+        assertEquals("Jorane", df.getColumnValues("Name").get(2));
+        assertEquals(23, df.getColumnValues("Age").get(2));
+        assertEquals("France", df.getColumnValues("Country").get(2));
+
+        assertEquals("Noemie", df.getColumnValues("Name").get(3));
+        assertEquals(23, df.getColumnValues("Age").get(3));
+        assertEquals("France", df.getColumnValues("Country").get(3));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithEmptyColumnLabels() {
+        String[] columnsLabels = {};
+        Object[][] rowsValues = {
+                {"Ali", 21, "Lebanon"},
+                {"Serge", 24, "Armenia"},
+                {"Jorane", 23, "France"},
+                {"Noemie", 23, "France"}
+        };
+        DataFrame df = new DataFrame(columnsLabels, rowsValues);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithEmptyRowsValues() {
+        String[] columnsLabels = {"Name", "Age", "Country"};
+        Object[][] rowsValues = {};
+        DataFrame df = new DataFrame(columnsLabels, rowsValues);
+    }
+
+    @Test
+    public void testConstructor2ParametersWithValidData() {
+        String[] columnsLabels = {"Name", "Age", "Country"};
+        Object[][] rowsValues = {
+                {"Ali", 21, "Lebanon"},
+                {"Serge", 24, "Armenia"},
+                {"Jorane", 23, "France"},
+                {"Noemie", 23, "France"}
+        };
+        DataFrame df = new DataFrame(columnsLabels, rowsValues);
+
+        assertEquals("Name", df.getColumnLabels().get(0));
+        assertEquals("Age", df.getColumnLabels().get(1));
+        assertEquals("Country", df.getColumnLabels().get(2));
+
+        assertEquals("Ali", df.getColumnValues("Name").get(0));
+        assertEquals(21, df.getColumnValues("Age").get(0));
+        assertEquals("Lebanon", df.getColumnValues("Country").get(0));
+
+        assertEquals("Serge", df.getColumnValues("Name").get(1));
+        assertEquals(24, df.getColumnValues("Age").get(1));
+        assertEquals("Armenia", df.getColumnValues("Country").get(1));
+
+        assertEquals("Jorane", df.getColumnValues("Name").get(2));
+        assertEquals(23, df.getColumnValues("Age").get(2));
+        assertEquals("France", df.getColumnValues("Country").get(2));
+
+        assertEquals("Noemie", df.getColumnValues("Name").get(3));
+        assertEquals(23, df.getColumnValues("Age").get(3));
+        assertEquals("France", df.getColumnValues("Country").get(3));
+    }
+
+}
