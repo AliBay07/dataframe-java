da7d3e12e1e8e515973f9e98efc722ab8de3f4fa
Noémie_Pellegrin <noemie.pellegrin@etu.univ-grenoble-alpes.fr>
2024-04-03 10:47:56 +0200

    Initialisation du pom avec ajout de Jacoco

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.*;
 public class MainTest {
 
     @Test
+    public void whenEmptyString_thenAccept() {
+        assertTrue(true);
+    }
+    @Test
     public void main() {
     }
 }
