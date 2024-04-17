5877c6f7528e6bd83786d0560b5fd0f4f81bb9e9
Serge Hakobyan <hakobyas@univ-grenoble-alpes.fr>
2024-04-17 20:35:30 +0200

    Terraform and Ansible config files added

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+  default = "devops-417719"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-b"
+}
+
+variable "instance-name" {
+  default = "instance-20240320-071445"
+}
+
